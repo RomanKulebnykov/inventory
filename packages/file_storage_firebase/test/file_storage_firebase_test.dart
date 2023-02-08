fa6ba81ab0c2714203db0dd55bab361d6581fb6d
Roman Kulebnykov <kulebnykov@icloud.com>
2023-02-08 16:23:50 +0200

    0.0.37

---
+++ packages/file_storage_firebase/test/file_storage_firebase_test.dart
@@ -0,0 +1,12 @@
+import 'package:file_storage_firebase/file_storage_firebase.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {});
+  });
+}
